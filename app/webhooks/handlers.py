0264f890ac8ed6865e7c9ab7a5262bac99918099
Chris Doehring <chrisdoehring@gmail.com>
2025-06-21 13:24:31 -0700

    Rough-in Survey123 handler.

---
+++ app/webhooks/handlers.py
@@ -0,0 +1,27 @@
+import json
+import pyjq
+import logging
+from app.services.gundi import send_observations_to_gundi, send_events_to_gundi
+from app.services.activity_logger import webhook_activity_logger
+from .core import WebhookPayload,  GenericJsonTransformConfig
+from pydantic import Field
+
+logger = logging.getLogger(__name__)
+
+class Survey123Payload(WebhookPayload):
+    _json: dict = Field(alias="json")
+
+
+@webhook_activity_logger()
+async def webhook_handler(payload: Survey123Payload, integration=None, webhook_config: GenericJsonTransformConfig = None):
+    logger.info(f"Webhook handler executed with integration: '{integration}'.")
+    logger.info(f"Payload: '{payload}'.")
+    logger.info(f"Config: '{webhook_config}'.")
+    input_data = json.loads(payload.json())
+    # filter_expression = webhook_config.jq_filter.replace("\n", "")
+    # transformed_data = pyjq.all(filter_expression, input_data)
+    logger.info(f"Transformed Data: {payload.dict()}")
+    
+    # TODO: Send data to Gundi
+    
+    return {"survey_items_count": 1}
---
+++ app/webhooks/models.py
@@ -0,0 +1,145 @@
+
+from typing import List
+
+from pydantic import BaseModel, Field
+from pydantic.config import BaseConfig, Extra
+
+class Attributes(BaseModel):
+    reported_time: int
+    globalid: str
+    
+    class Config:
+        allow_population_by_field_name = True
+        extra = Extra.allow
+
+    # notes: str
+    # radness: str
+    # species: str
+    
+
+class SpatialReference(BaseModel):
+    wkid: int
+
+
+class Geometry(BaseModel):
+    x: float
+    y: float
+    spatialReference: SpatialReference
+
+
+class Result(BaseModel):
+    globalId: str
+    objectId: int
+    success: bool
+    uniqueId: int
+
+
+class LayerInfo(BaseModel):
+    id: int
+    name: str
+
+
+class Feature(BaseModel):
+    attributes: Attributes
+    geometry: Geometry
+    result: Result
+    layerInfo: LayerInfo
+
+
+class Attributes1(BaseModel):
+    notes: str
+    radness: str
+    reported_time: int
+    species: str
+    globalid: str
+
+
+class SpatialReference1(BaseModel):
+    wkid: int
+
+
+class Geometry1(BaseModel):
+    x: float
+    y: float
+    spatialReference: SpatialReference1
+
+
+class Result1(BaseModel):
+    globalId: str
+    objectId: int
+    success: bool
+    uniqueId: int
+
+
+class LayerInfo1(BaseModel):
+    id: int
+    name: str
+
+
+class Add(BaseModel):
+    attributes: Attributes1
+    geometry: Geometry1
+    result: Result1
+    layerInfo: LayerInfo1
+
+
+class LayerInfo2(BaseModel):
+    id: int
+    name: str
+    type: str
+    objectIdField: str
+    globalIdField: str
+    relationships: List
+
+
+class ApplyEdit(BaseModel):
+    id: int
+    adds: List[Add]
+    layerInfo: LayerInfo2
+
+
+class AddResult(BaseModel):
+    globalId: str
+    objectId: int
+    success: bool
+    uniqueId: int
+
+
+class ResponseItem(BaseModel):
+    addResults: List[AddResult]
+    id: int
+
+
+class SurveyInfo(BaseModel):
+    formItemId: str
+    formTitle: str
+    serviceItemId: str
+    serviceUrl: str
+
+
+class PortalInfo(BaseModel):
+    url: str
+    token: str
+
+
+class UserInfo(BaseModel):
+    username: str
+    firstName: str
+    lastName: str
+    fullName: str
+    email: str
+
+
+class Payload(BaseModel):
+    eventType: str
+    feature: Feature
+    applyEdits: List[ApplyEdit]
+    response: List[ResponseItem]
+    surveyInfo: SurveyInfo
+    portalInfo: PortalInfo
+    userInfo: UserInfo
+
+
+class Survey123Payload(BaseModel):
+    _json: Payload = Field(alias="json")
+
